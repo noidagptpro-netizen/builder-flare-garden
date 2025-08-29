db10b887234f3b862d4175306f0935e5e6691976
noidagptpro-netizen <noidagptpro@gmail.com>
2025-08-29 23:47:53 +0530

    Add PayUCheckout component

---
+++ src/payments/PayUCheckout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PayUCheckout = () => {
+    return (
+        <div>
+            <h1>PayU Checkout Component</h1>
+            {/* Add your checkout logic here */}
+        </div>
+    );
+};
+
+export default PayUCheckout;
